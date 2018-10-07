5e43d1f61c70485fe4629ec2221f67572473067d
Alex Tait <alexander.tait@nist.gov>
2018-10-07 16:52:28 -0600

    restructured as a salt package

---
+++ klayout_dot_config/python/README.md
@@ -0,0 +1,2 @@
+# lygadgets
+This is the short readme. For the full one, see the corresponding klayout salt package
--- klayout_dot_config/python/lygadgets/__init__.py
+++ klayout_dot_config/python/lygadgets/__init__.py
@@ -1,5 +1,5 @@
 from .environment import pya, isGUI, isGSI
 from .messaging import message, message_loud
 from .markup import xml_to_dict
-from .sourcery import export_to_system
-from .lypackage_linker import post_install_factory
+from .salt_linker import export_to_system
+from .system_linker import post_install_factory
