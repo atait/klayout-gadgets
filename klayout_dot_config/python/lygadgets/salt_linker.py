f3c12a19f916fd880908b2c0f7f1290be17df203
Alex Tait <alexander.tait@nist.gov>
2018-10-08 14:43:39 -0600

    made a linker for pure python

--- klayout_dot_config/python/lygadgets/salt_linker.py
+++ klayout_dot_config/python/lygadgets/salt_linker.py
@@ -17,6 +17,18 @@ def is_windows():
         raise ValueError('Unrecognized operating system: {}'.format(platform))
 
 
+def klayout_home():
+    ''' Figure out the klayout configuration directory.
+        Uses exactly the same logic as used in the klayout source.
+    '''
+    ly_home = os.environ.get('KLAYOUT_HOME', '')
+    if ly_home == '':
+        ly_home = os.path.join(os.path.expanduser('~'), 'KLayout' if is_windows() else '.klayout')
+    if not os.path.exists(ly_home):
+        raise FileNotFoundError('The KLayout config directory was not found. KLayout might not be installed.')
+    return ly_home
+
+
 def symlink_windows(source, destination):
     # API based. The easy way
     try:
@@ -53,13 +65,29 @@ def symlink_windows(source, destination):
     raise WindowsError('Failed to find a way to link to a file in windows')
 
 
-def link_to_salt(lypackage_dir):
-    # Figure out the klayout configuration directory
-    klayout_home = os.path.join(os.path.expanduser('~'), 'KLayout' if is_windows() else '.klayout')
-    if not os.path.exists(klayout_home):
-        raise FileNotFoundError('The KLayout config directory was not found. KLayout might not be installed.')
+def attempt_symlink(source, dest, overwrite=False):
+    ''' Platform independent. Returns the destination if the link was created, otherwise None
+    '''
+    if source == dest:
+        # Prevent circular reference
+        return None
+    if os.path.exists(dest):
+        if not overwrite:
+            return None
+        else:
+            # remove
+    if not is_windows():
+        os.symlink(source, dest)
+    else:
+        symlink_windows(source, dest)
+    return dest
 
-    salt_dir = os.path.join(klayout_home, 'salt')
+
+def link_to_salt(lypackage_dir):
+    '''
+        Attempts to link the lypackage_dir into salt.
+    '''
+    salt_dir = os.path.join(klayout_home(), 'salt')
     if not os.path.exists(salt_dir):
         os.mkdir(salt_dir)
 
@@ -69,19 +97,24 @@ def link_to_salt(lypackage_dir):
     salt_link = os.path.join(salt_dir, registered_name)
 
     # Make the symlink
-    if os.path.exists(salt_link):
-        return None
-    else:
-        if not is_windows():
-            os.symlink(lypackage_dir, salt_link)
-        else:
-            symlink_windows(lypackage_dir, salt_link)
-        return salt_link
+    return attempt_symlink(lypackage_dir, salt_link, overwrite=False)
 
 
-def post_install_factory(klayout_dot_config_dir):
+def link_to_user_python(package_dir):
+    '''
+        Attempts to link the package_dir into klayout's standalone python directory.
+    '''
+    python_dir = os.path.join(klayout_home(), 'python')
+    if not os.path.exists(python_dir):
+        os.mkdir(python_dir)
+    package_name = os.path.basename(package_dir)
+    link = os.path.join(python_dir, package_name)
+    return attempt_symlink(package_dir, link, overwrite=False)
+
+
+def postinstall_factory(source, linker_function):
     ''' Generates a class that subclasses install.
-        It attempts to link the klayout_dot_config_dir into salt.
+        It attempts to link the lypackage_dir into salt.
         It can be run by setup.py with the argument::
 
             cmdclass={'install': post_install_factory(some_directory)},
@@ -90,11 +123,22 @@ def post_install_factory(klayout_dot_config_dir):
         def run(self):
             install.run(self)
             try:
-                the_link = link_to_salt(klayout_dot_config_dir)
+                the_link = linker_function(source)
                 if the_link is not None:
-                    print('\nAutoinstall into klayout salt succeeded!\n{}\n'.format(the_link))
+                    print('\nAutoinstall into klayout succeeded!\n{}\n'.format(the_link))
+                else:
+                    print('Folder already present. Not overwriting.')
             except Exception as err:
-                print('Autoinstall into klayout salt failed with the following\n')
+                print('Autoinstall into klayout failed with the following\n')
                 print(err)
-                print('\nYou must perform a manual install as described in the README.')
+                print('\nYou must perform a manual install as described in a README.')
     return PostInstall
+
+
+def postinstall_lypackage(lypackage_dir):
+    return postinstall_factory(lypackage_dir, link_to_salt)
+
+
+def postinstall_pure(package_dir):
+    return postinstall_factory(package_dir, link_to_user_python)
+
