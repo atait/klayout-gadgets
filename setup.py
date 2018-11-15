4a34323199538171355843844824c924ecd06f6d
Alex Tait <alexander.tait@nist.gov>
2018-11-14 21:13:46 -0700

    version bump 1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.5',
+      version='0.1.6',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       author='Alex Tait',
