7ca224ad5da24b09ada42aba54021d342124397c
Alex Tait <alexander.tait@nist.gov>
2018-11-14 20:49:29 -0700

    travis round 4

--- lygadgets/__init__.py
+++ lygadgets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from lygadgets.environment import pya, isGUI, isGSI, patch_environment
 from lygadgets.messaging import message, message_loud
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.4',
+      version='0.1.5',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       author='Alex Tait',
