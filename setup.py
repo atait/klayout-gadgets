8840cf8d8cef24ec90a1671b76e228991e23c971
Alex Tait <atait@princeton.edu>
2018-12-05 11:23:26 -0700

    version bump 1.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.7',
+      version='0.1.8',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       author='Alex Tait',
