0fb9248c47021bf697dccaddc23c02dfca7ff3f3
Alex Tait <atait@princeton.edu>
2019-01-15 16:47:49 -0700

    new function lyp_to_layerlist

--- lygadgets/__init__.py
+++ lygadgets/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.1.12'
 
 from lygadgets.environment import pya, isGUI, isGSI, patch_environment, klayout_home
 from lygadgets.messaging import message, message_loud
-from lygadgets.markup import xml_to_dict
+from lygadgets.markup import xml_to_dict, lyp_to_layerlist
 from lygadgets.system_linker import export_to_system
 from lygadgets.salt_linker import postinstall_hook
 from lygadgets.technology import Technology
--- lygadgets/markup.py
+++ lygadgets/markup.py
@@ -1,4 +1,12 @@
 import xmltodict
 
 def xml_to_dict(xml_content):
+    # to be deprecated
     return xmltodict.parse(xml_content, process_namespaces=True)
+
+
+def lyp_to_layerlist(lyp_file):
+    with open(filename, 'r') as fx:
+        lyp_dict = xmltodict.parse(fx.read(), process_namespaces=True)
+    all_layers = lyp_dict['layer-properties']['properties']
+    return all_layers
