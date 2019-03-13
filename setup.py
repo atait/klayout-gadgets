d83b9dbc74b18d165754869b489e48d002511087
Alex Tait <atait@princeton.edu>
2019-03-13 15:42:54 -0600

    version 1.24

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.23',
+      version='0.1.24',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       author='Alex Tait',
