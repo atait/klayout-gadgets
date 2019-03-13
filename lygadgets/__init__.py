87a76c9e60f5c803c7a3f4ca88ea8c1ae0725d43
Alex Tait <atait@princeton.edu>
2019-03-12 23:54:39 -0600

    version bump 1.21

--- lygadgets/__init__.py
+++ lygadgets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.20'
+__version__ = '0.1.21'
 
 from lygadgets.environment import pya, isGUI, isGSI, patch_environment, klayout_home
 from lygadgets.messaging import message, message_loud
