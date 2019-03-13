a084500d9f110ff17c5e323868ff58d3eb9cf09b
Alex Tait <atait@princeton.edu>
2019-03-12 22:57:38 -0600

    version bump 1.20

--- lygadgets/__init__.py
+++ lygadgets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.19'
+__version__ = '0.1.20'
 
 from lygadgets.environment import pya, isGUI, isGSI, patch_environment, klayout_home
 from lygadgets.messaging import message, message_loud
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.19',
+      version='0.1.20',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       author='Alex Tait',
