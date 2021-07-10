9bfa365a17822dc8f142a191e8fbdd8ce6a6fac5
Alex Tait <atait@ieee.org>
2021-07-10 14:34:34 -0600

    version bump 1.31

--- lygadgets/__init__.py
+++ lygadgets/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.30'
+__version__ = '0.1.31'
 __lygadget_link__ = ['future', 'xmltodict']
 
 from lygadgets.environment import pya, isGUI, isGSI, patch_environment, klayout_home
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.30',
+      version='0.1.31',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       long_description_content_type='text/markdown',
