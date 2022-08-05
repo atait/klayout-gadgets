a78a6b907ecb0ce1b3d6438831345a3f472a3530
Alex Tait <atait@ieee.org>
2022-08-05 16:33:25 -0400

    version bump 1.35

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='lygadgets',
-      version='0.1.34',
+      version='0.1.35',
       description='Tools to make klayout, the standalone, and python environments work better together',
       long_description=readme(),
       long_description_content_type='text/markdown',
